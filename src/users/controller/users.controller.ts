a3ee9424e1fce7d116e76cfff35ba3f1aa4a116d
Levy <levyronald3@gmail.com>
2022-09-02 10:49:57 -0300

    fix: removendo imports desnecessarios

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,15 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
-import { IsPublic } from 'src/auth/decorators/is-public.decorator';
-
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('/create')
-  @IsPublic()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
