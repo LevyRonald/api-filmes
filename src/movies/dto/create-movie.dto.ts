de41bf243451a97cb1979bb514b063a027febf03
Levy <levyronald3@gmail.com>
2022-09-06 16:24:01 -0300

    feat: adicionando validação na collection movies

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,20 +1,57 @@
+import { IsArray, IsDate, IsDateString, IsEmpty, IsNumber, IsObject, IsString } from "class-validator"
+
 export class CreateMovieDto {
+    @IsString()
     plot: string
+
+    @IsArray()
     genres: Array<string>
+
+    @IsNumber()
     runtime: number
+
+    @IsArray()
     cast: Array<string>
+
+    @IsString()
     poster: string
+
+    @IsString()
     title: string
+
+    @IsString()
     fullplot: string
+
+    @IsArray()
     languages: Array<string>
+
+    @IsDateString()
     released: Date
+
+    @IsArray()
     directors: Array<string>
+
+    @IsString()
     rated: string
+
+    @IsObject()
     awards: Object
+
+    @IsDateString()
     lastupdated: Date
+
+    @IsNumber()
     year: number
+
+    @IsObject()
     imdb: object
+
+    @IsArray()
     countries: Array<string>
+
+    @IsString()
     type: string
+
+    @IsObject()
     tomatoes: Object
 }
