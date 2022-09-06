dd959de2c5c230666633cbb78f996ba732d7a54c
Levy <levyronald3@gmail.com>
2022-09-06 11:10:39 -0300

    fix: alterando ids de session para string

--- src/sessions/services/sessions.service.ts
+++ src/sessions/services/sessions.service.ts
@@ -18,15 +18,15 @@ export class SessionsService {
     return this.session.find();
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} session`;
   }
 
-  update(id: number, updateSessionDto: UpdateSessionDto) {
+  update(id: string, updateSessionDto: UpdateSessionDto) {
     return `This action updates a #${id} session`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} session`;
   }
 }
