0b4185515e18c58348d104cada435d555de59a64
Levy <levyronald3@gmail.com>
2022-09-06 16:28:57 -0300

    feat: adicionando validação na collection comments

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,9 +1,19 @@
+import { IsDateString, IsEmail, IsString } from "class-validator"
 import { ObjectId } from "mongoose"
 
 export class CreateCommentDto {
+    @IsString()
     name: string
+
+    @IsEmail()
     email: string
+
+    @IsString()
     movie_id: ObjectId
+
+    @IsString()
     text: string
+
+    @IsDateString()
     date: Date
 }
